a97e83f6dc058c9dbd72b463a50bb8f92ec91347
qaser <huji@mail.ru>
2025-03-26 19:44:09 +0500

    fix

--- dialogs/for_report/selected.py
+++ dialogs/for_report/selected.py
@@ -22,23 +22,6 @@ def vehicle_getter(data) -> list[tuple[str, str]]:
 
 async def on_vehicle_selected(callback: CallbackQuery, select: Select,
                             manager: DialogManager, item_id: str):
-    data = await getters.get_report_data(manager)
-    selected_vehicle = item_id
-    report_lines = data["result_by_vehicle"].get(selected_vehicle, [])
-
-    report_text = (f"<b>Заявки на {selected_vehicle} {data['date']} "
-                  f"по состоянию на {data['current_time']}(мск):</b>\n\n")
-    report_text += "\n".join(report_lines)
-
-    await callback.message.answer(
-        text=report_text,
-        parse_mode='HTML',
-    )
-    await manager.done()
-
-
-async def on_vehicle_selected(callback: CallbackQuery, select: Select,
-                            manager: DialogManager, item_id: str):
     data = await getters.get_vehicle_window_data(manager)
     selected_vehicle = item_id
 
@@ -78,6 +61,23 @@ async def on_location_selected(callback: CallbackQuery, widget: Radio,
 
     await callback.message.answer(
         text=report_text,
+        parse_mode='HTML',
+    )
+    await manager.done()
+
+
+async def on_full_report_selected(callback: CallbackQuery, button: Button, manager: DialogManager):
+    data = await getters.get_full_report_data(manager)
+    if not data["has_orders"]:
+        await callback.message.answer("Заявки на технику пока отсутствуют")
+        await manager.done()
+        return
+    final_message = '{}\n\n{}'.format(
+        f'Заявки на технику {data["date"]} по состоянию на {data["current_time"]}(мск):',
+        data["full_report_text"],
+    )
+    await callback.message.answer(
+        text=final_message,
         parse_mode='HTML',
     )
     await manager.done()
